6ec87198df04c46e41031045db0f6735e8ca3b9e
Daisy Sylvia <66061010+daisylvia@users.noreply.github.com>
2022-06-24 08:42:50 +0300

    Add files via upload

---
+++ name.js
@@ -0,0 +1,6 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+<script>
+console.log('Daisy Sylvia Akinyi')
+console.log('Height: 167cm');
+console.log('Country: Kenya');
+</script>
